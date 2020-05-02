21d07fb481df84113c56d5eec30833840725f411
K-HyungHoon <48951879+K-HyungHoon@users.noreply.github.com>
2020-05-02 18:52:46 +0900

    Update device.sh

--- src/device.sh
+++ src/device.sh
@@ -11,19 +11,17 @@ for interface in `iw dev | grep Interface | cut -f 2 -s -d" "`
 do
   # for each interface, get mac addresses of connected stations/clients
   mac_list=`iw dev $interface station dump | grep Station | cut -f 2 -s -d" "`
-  # echo -e "\nMAC LIST: $mac_list\n"
   
   # for each mac address in that list
   for mac in $mac_list
   do
-    # echo "MAC: $mac"
     arp_ip=`echo "$arp_table" | grep $mac | awk '{print $1}'`
     
     for ip in $arp_ip
     do
-      # echo "IP: $ip"
       # if a DHCP Lease has been given out by dnsmasq, save it
       dhcp_host=`cat /tmp/dhcp.leases | cut -f 2,3,4 -s -d" " | grep $ip | cut -f 3 -s -d" "`
+      
       if [[ $dhcp_host ]]
       then
         # show wifi client
